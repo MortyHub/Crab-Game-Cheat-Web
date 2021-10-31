84db17a95b6db2659ca9ac1321a4fff21ee4da53
Sarthing Gamer <83893877+MortyHub@users.noreply.github.com>
2021-10-31 20:05:31 +0000

    Committing to Listing of users online to the cheat.

---
+++ script.js
@@ -0,0 +1,38 @@
+var count;
+var status;
+var users = [];
+
+function check(x) {
+	if(x == 'count')
+	{
+
+	} else
+		{
+			if(x == 'status')
+			{
+
+			} else
+				{
+					if(x == 'users')
+					{
+
+					}
+				}
+		}
+}
+
+setInterval(function()
+{
+	document.getElementById('server-status').innerHTML = 'Server Status: ' + status;
+	document.getElementById('count').innerHTML = 'Users Online: ' + count;
+}, 1)
+for(let i = 0; i < users.length; i++)
+{
+ var Elem = document.createElement("h5");
+ Elem.innerText = users[i];
+ Elem.style.color = 'white';
+ document.getElementById('center-user').appendChild(Elem);
+ 
+}
+
+
