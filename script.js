42fa63d438eda29dd596a6485bd02b7b05317cea
Sarthing Gamer <83893877+MortyHub@users.noreply.github.com>
2021-11-02 00:24:14 +0000

    Added script to tell the website to list when a user is using the cheat

--- script.js
+++ script.js
@@ -25,14 +25,11 @@ setInterval(function()
 {
 	document.getElementById('server-status').innerHTML = 'Server Status: ' + status;
 	document.getElementById('count').innerHTML = 'Users Online: ' + count;
-}, 1)
+}, 1);
 for(let i = 0; i < users.length; i++)
 {
  var Elem = document.createElement("h5");
  Elem.innerText = users[i];
  Elem.style.color = 'white';
  document.getElementById('center-user').appendChild(Elem);
- 
 }
-
-
